aee1931846ca837961803def1f10c8f654c99154
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-02-19 19:20:59 +0900

    wip scalajs-bundler

--- build.sbt
+++ build.sbt
@@ -4,11 +4,21 @@ ThisBuild / organizationName := "NomadBlacky"
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 lazy val lambda = (project in file("."))
-  .enablePlugins(ScalaJSPlugin)
+  .enablePlugins(ScalaJSBundlerPlugin)
   .settings(
     name := "lambda",
     libraryDependencies ++= Seq(
-      "net.exoego" %%% "aws-lambda-scalajs-facade" % "0.3.3"
+      "net.exoego"                   %%% "aws-lambda-scalajs-facade" % "0.5.0",
+      "com.softwaremill.sttp.client" %%% "core"                      % "2.0.0-RC11",
+      "org.scalatest"                %%% "scalatest"                 % "3.1.0" % Test
+    ),
+    Compile / npmDependencies ++= Seq(
+      "node-fetch"               -> "2.6.0",
+      "abortcontroller-polyfill" -> "1.4.0",
+      "fetch-headers"            -> "2.0.0"
+    ),
+    Compile / npmDevDependencies ++= Seq(
+      "serverless" -> "1.64.0"
     ),
     scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) }
   )
--- src/main/scala/Handler.scala
+++ src/main/scala/Handler.scala
@@ -1,18 +1,31 @@
 import net.exoego.facade.aws_lambda._
+import sttp.client._
 
-import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.Promise
-import scala.scalajs.js.annotation.JSExportTopLevel
+import scala.scalajs.js.annotation.{JSExportAll, JSExportTopLevel}
 
+@JSExportTopLevel("Handler")
+@JSExportAll
 object Handler {
-  @JSExportTopLevel("handleRequest")
+  implicit val sttpBackend = FetchBackend()
+
+  // https://sttp.readthedocs.io/en/latest/backends/javascript/fetch.html#node-js
+  //private val g = scalajs.js.Dynamic.global
+  //g.fetch = g.require("node-fetch")
+  //g.require("abortcontroller-polyfill/dist/polyfill-patch-fetch")
+  //g.Headers = g.require("fetch-headers")
+
   def handleRequest(
       request: APIGatewayProxyEvent,
       context: Context
-  ): Promise[APIGatewayProxyResult] =
-    Future {
-      APIGatewayProxyResult(200, "okkkkkkkkkey")
-    }.toJSPromise
+  ): Promise[APIGatewayProxyResult] = {
+    val future = for {
+      res <- quickRequest.get(uri"https://httpbin.org/get").send()
+    } yield {
+      APIGatewayProxyResult(200, res.body)
+    }
+    future.toJSPromise
+  }
 }
